209aeb70cdfb21390a029b75e78710488fe3a614
Dewey Dunnington <dewey@fishandwhistle.net>
2016-11-10 11:45:13 -0400

    rename to plotwp, add license, remove unnecessary files

---
+++ plotwp.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @package Plotwp
+ * @version 0.1
+ */
+/*
+Plugin Name: Plot.wp
+Plugin URI: http://github.com/paleolimbot/plotwp
+Description: Add JSON-based plots to posts and pages using the <a href="https://plot.ly/javascript/">plotly.js</a> API
+Author: Dewey Dunnington
+Version: 0.1
+Author URI: http://www.fishandwhistle.net/
+*/
+
+/*
+ * Add the plot.ly JS library and defaultplot.css to the header
+ */
+function plotwp_enqueue_scripts() {
+    wp_enqueue_script( 'plot.ly', 'https://cdn.plot.ly/plotly-latest.min.js' , false );
+    wp_enqueue_style('plotwp_default', plugins_url('defaultplot.css', __FILE__), false);
+}
+
+add_action( 'wp_enqueue_scripts', 'plotwp_enqueue_scripts' );
+
+/*
+ * Add the [plotly] shortcode
+ */
+$plotwp_plotly_div_id = 1;
+function plotwp_plotly_shortcode( $atts, $content = null ) {
+    global $plotwp_plotly_div_id;
+    
+    if(empty($content)) {
+        return "";
+    }
+    $json = json_decode($content);
+    if(empty($json)) {
+        return "<b>Invalid JSON in plotly shortcode</b>";
+    }
+    
+    if(!is_array($atts)) {
+        $atts = array();
+    }
+    $divatts = join(' ', array_map(function($key) use ($atts) {
+            return $key.'="'. esc_attr($atts[$key]).'"';
+        }, array_keys($atts)));
+    
+    $plotly_div_id = 'plotwp_plotly_' . $plotwp_plotly_div_id++;
+    return '<div ' . $divatts . ' id="' . $plotly_div_id . '"></div>
+    <script type="text/javascript">
+        Plotly.plot( document.getElementById("' . $plotly_div_id . '"), ' . json_encode($json) . ' );
+    </script>';
+}
+add_shortcode( 'plotly', 'plotwp_plotly_shortcode' );
+
+function plotwp_shortcodes_to_exempt_from_wptexturize( $shortcodes ) {
+    $shortcodes[] = 'plotly';
+    return $shortcodes;
+}
+add_filter( 'no_texturize_shortcodes', 'plotwp_shortcodes_to_exempt_from_wptexturize' );
+remove_filter( 'the_content', 'wpautop' );
+add_filter( 'the_content', 'wpautop' , 99);
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Plot.wp ===
+Contributors: paleolimbot
+Tags: charts, graphs, plot, plots, scientific
+Requires at least: 3.7
+Tested up to: 4.6.1
+Stable tag: 0.1
+License: GPLv2 or later
+Donate link: http://apps.fishandwhistle.net/donate
+
+Add JSON-based plots to posts and pages using the [plotly](https://plot.ly/javascript/) API
+
+== Description ==
+
+Plot.wp enables the [plotly] shortcode, allowing easy inclusion of JSON-based plots using the [plotly](https://plot.ly/javascript/) API. Just add valid plotly JSON between the `[plotly]` and `[/plotly]` shortcodes, and revel as an interactive plot appears in your post.
+
+== Installation ==
+
+Upload the Plot.wp plugin to your blog, Activate it, then use the `[plotly]...[/plotly]` shortcode to start plotting!
+
+== Frequently Asked Questions ==
+
+= How do I add a plot to my post/page? =
+
+Plots are added using the `[plotly]...[/plotly]` shortcode. In between the shortcode must be a valid JSON object that will be passed to `Plotly.plot('myDiv', ...)`.
+
+```
+[plotly]
+{
+  "data": [{
+    "x": [1, 2, 3, 4, 5],
+    "y": [27, 28, 29, 50],
+    "mode": "lines+markers",
+    "type": "scatter"
+  }],
+  "layout": {
+    "margin": {
+      "t": 40, "r": 40, "b": 40, "l":40
+    }
+  }
+}
+[/plotly]
+```
+
+For more information about the [plotly](http://plot.ly).js API, see the [online documentation](https://plot.ly/javascript/).
+
+== Changelog ==
+
+= 0.1 =
+*Release Date - 10 November 2016*
+* First release
